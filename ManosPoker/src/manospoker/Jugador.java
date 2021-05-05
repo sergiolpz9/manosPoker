4c8220c166911e8fa9e113e9f46201d8f5264db9
Sergio <sergio.lj.gr@gmail.com>
2021-05-05 22:05:42 +0200

    Update Jugador.java

--- ManosPoker/src/manospoker/Jugador.java
+++ ManosPoker/src/manospoker/Jugador.java
@@ -16,10 +16,14 @@ public class Jugador {
     
     private String id_;
     private List<Carta> mano_;
+    private int puntuacion_;
+    private int puntuacionDesempate_;
     
-    public Jugador(String id, int nCartas){
+    public Jugador(String id){
         id_=new String(id);
         mano_= new ArrayList();
+        puntuacion_=0;
+        puntuacionDesempate_=0;
         
     }
     
@@ -31,4 +35,39 @@ public class Jugador {
       mano_.clear();
     }
     
+   public void printMano(){
+       //método para imprimir en pantalla la mano del jugador
+   }
+   public void ordenarCartas(){
+       //método para ordenar las cartas de mayor a menor valor
+           
+       
+   }
+   public boolean escalera(){
+       this.ordenarCartas();
+       boolean salida=true;
+       for(int i=1;i<mano_.size();i++){
+           if(mano_.get(i).getValor()!=(mano_.get(i-1).getValor()+1)){
+               salida=false;
+           }
+       }
+       
+       return salida;
+   }
+   
+   public boolean color(){
+       boolean salida=true;
+       for (int i=0; i<(mano_.size()-1); i++) {
+            if (!(mano_.get(i).getPalo().equals(mano_.get(i+1).getPalo()))) {
+                salida=false;
+            }
+        }
+       return salida;
+   }
+   
+   public boolean escaleraColor(){
+        return ((this.escalera()) && (this.color()));
+   }
+
+
 }
